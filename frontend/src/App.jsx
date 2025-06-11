4b98ca27877c4e0ae92bf7bd7be037aa790847ee
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-06-11 22:14:12 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from './routes/Home';
+import AdminLogin from './routes/AdminLogin';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' Component={Home}/>
+        <Route path='/admin/login' Component={AdminLogin}/>
       </Routes>
     </BrowserRouter>
   )
