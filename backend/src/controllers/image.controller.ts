618f93c84fc75afcdab18175d767ef1df5f21c04
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-06-11 23:48:33 +0530

    image controller and image routes added

--- backend/src/controllers/image.controller.ts
+++ backend/src/controllers/image.controller.ts
@@ -2,34 +2,39 @@ import { deleteImage, uploadImageURL } from "../s3";
 import asyncHandler from "../utils/asyncHandler";
 import { AppError } from "../utils/error";
 import response from "../utils/response";
-import {Request,Response} from "express";
+import { Request, Response } from "express";
 
 /**
- * 
+ *
  * @description Upload an image to cloud storage
  * @route POST /api/image/uploadURL
  * @access Private
- * @param req 
- * @param res  
+ * @param req
+ * @param res
  */
 export const uploadImage = asyncHandler(async (req: Request, res: Response) => {
-    const {imageName}=req.body;
-    if(!imageName) throw new AppError("imageName is required!",400);
-    const uploadUrl=await uploadImageURL(imageName);
-	response(res, 201, "image url created successfully", { imageUploadingUrl: uploadUrl });
+	const { imageName } = req.body;
+	if (!imageName) throw new AppError("imageName is required!", 400);
+	const uploadUrl = await uploadImageURL(imageName);
+	response(res, 201, "image url created successfully", {
+		imageUploadingUrl: uploadUrl,
+		imageUrl: uploadUrl.split("?")[0],
+	});
 });
 
 /**
- * 
+ *
  * @description Upload an image to cloud storage
- * @route POST /api/image/deleteImage
+ * @route DELETE /api/image/deleteImage
  * @access Private
- * @param req 
- * @param res  
+ * @param req
+ * @param res
  */
-export const deleteUploadedImage = asyncHandler(async (req: Request, res: Response) => {
-    const {imageURL}=req.body;
-    if(!imageURL) throw new AppError("imageURL is required!",400);
-    await deleteImage(imageURL);
-	response(res, 201, "image deleted successfully", {});
-});
+export const deleteUploadedImage = asyncHandler(
+	async (req: Request, res: Response) => {
+		const { imageURL } = req.body;
+		if (!imageURL) throw new AppError("imageURL is required!", 400);
+		await deleteImage(imageURL);
+		response(res, 201, "image deleted successfully", {});
+	}
+);
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import userRouter from "./user.routes";
+import imageRouter from "./image.routes";
 const router = Router();
 
-router.use("/api/v1", userRouter);
+router.use("/user", userRouter);
+router.use("/image", imageRouter);
 
 export default router;
