ca3ddec925de404b6e1f96c1a41d5d8a6001d5bb
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-06-12 16:13:45 +0530

    formatted code

--- backend/src/routes/photoGallery.routes.ts
+++ backend/src/routes/photoGallery.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { addImage, deleteGalleryImage, getAllImages } from "../controllers/photoGallery.controller";
+import {
+	addImage,
+	deleteGalleryImage,
+	getAllImages,
+} from "../controllers/photoGallery.controller";
 import { auth } from "../middlewares/auth.middleware";
 const photoGalleryRouter = Router();
 
 photoGalleryRouter
-	.post("/addImage",auth, addImage)
+	.post("/addImage", auth, addImage)
 	.get("/getImages", getAllImages)
 	.delete("/deleteImage", auth, deleteGalleryImage);
 
