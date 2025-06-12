b6a4eeec1c7e759add24314ad59d68dbe9593bbc
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-06-12 11:00:11 +0530

    auth middleware added

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -15,27 +15,21 @@ export interface ITokenPayload {
  * @throws {AppError} - Throws an error if the token is not provided or is invalid
  * @description - This middleware function checks for a valid JWT token in the request headers or body. If the token is valid, it adds the user data to the request object and calls the next middleware. If the token is invalid or not provided, it throws an AppError with a 401 status code.
  */
-const auth = () => {
-	return asyncHandler(
-		async (
-			req: Request,
-			res: Response,
-			next: NextFunction
-		): Promise<void> => {
-			const token =
-				req.headers?.["token"] ||
-				req.body?.["token"] ||
-				req.params?.["token"] ||
-				req.query?.["token"] ||
-				req.headers.authorization?.split(" ")[1] ||
-				"";
-			if (!token) throw new AppError("Unauthorised", 401);
-			const data = await verifyToken(token);
-			req.user = data;
-			next();
-		}
-	);
-};
+const auth = asyncHandler(
+	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+		const token =
+			req.headers?.["token"] ||
+			req.body?.["token"] ||
+			req.params?.["token"] ||
+			req.query?.["token"] ||
+			req.headers.authorization?.split(" ")[1] ||
+			"";
+		if (!token) throw new AppError("Unauthorised", 401);
+		const data = await verifyToken(token);
+		req.user = data;
+		next();
+	}
+);
 
 /**
  * Creates a JWT token with the provided data and expiration time
