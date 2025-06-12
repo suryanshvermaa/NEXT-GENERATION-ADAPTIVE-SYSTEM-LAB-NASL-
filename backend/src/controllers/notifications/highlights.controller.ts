4696897146917367b83e8c944b8431badeb812e4
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-06-12 18:20:22 +0530

    added highlight controller

--- backend/src/controllers/notifications/highlights.controller.ts
+++ backend/src/controllers/notifications/highlights.controller.ts
@@ -3,8 +3,6 @@ import { Request, Response } from "express";
 import { AppError } from "../../utils/error";
 import prisma from "../../config/db";
 import response from "../../utils/response";
-import { deleteImage } from "../../s3";
-
 /**
  *
  * @description create highlight
@@ -23,48 +21,19 @@ export const createHighlight = asyncHandler(
 				highlight,
 				description,
 				date,
-                link,
+				link,
 			},
 		});
 		if (!newHighlight)
 			throw new AppError("Error in creating highlight", 400);
-		response(res, 201, "highlight created", { highlight:newHighlight });
+		response(res, 201, "highlight created", { highlight: newHighlight });
 	}
 );
 
 /**
  *
- * @description update research area
- * @route POST /api/highlight/updatehighlight
- * @access Private
- * @param req
- * @param res
- */
-export const updatehighlight = asyncHandler(
-	async (req: Request, res: Response) => {
-		const { name, description, imageURL, id } = req.body;
-		if (!id || !name || !description || !imageURL)
-			throw new AppError("all fields are required", 400);
-		const updatedhighlight = await prisma.highlight.update({
-			where: {
-				id: id as number,
-			},
-			data: {
-				imageURL,
-				name,
-				description,
-			},
-		});
-		if (!updatedhighlight)
-			throw new AppError("Error in updating highlight", 400);
-		response(res, 201, "research Area updated", { updatedhighlight });
-	}
-);
-
-/**
- *
- * @description delete research area
- * @route POST /api/highlight/deletehighlight
+ * @description delete highlight
+ * @route POST /api/highlight/delete
  * @access Private
  * @param req
  * @param res
@@ -73,58 +42,32 @@ export const deletehighlight = asyncHandler(
 	async (req: Request, res: Response) => {
 		const { id } = req.body;
 		if (!id) throw new AppError("Id is required", 400);
-		const highlight = await prisma.highlight.delete({
+		await prisma.highlight.delete({
 			where: {
 				id,
 			},
 		});
-		await deleteImage(highlight.imageURL);
 		response(res, 200, "highlight deleted successfully", {});
 	}
 );
 
 /**
  *
- * @description fetching research area
- * @route POST /api/highlight/gethighlight?id="idofhighlight"
+ * @description fetching highlights
+ * @route GET /api/highlight/getHighlights
  * @access Private
  * @param req
  * @param res
  */
-export const gethighlight = asyncHandler(
-	async (req: Request, res: Response) => {
-		const { id } = req.query;
-		if (!id) throw new AppError("id is required", 400);
-		const highlight = await prisma.highlight.findUnique({
-			where: {
-				id: Number(id),
-			},
-		});
-		if (!highlight) throw new AppError("highlight not found", 400);
-		response(res, 200, "highlight fetched successfully", {
-			highlight,
-		});
-	}
-);
-
-/**
- *
- * @description getReasearchAreas
- * @route POST /api/highlight/getReasearchAreas
- * @access Private
- * @param req
- * @param res
- */
-export const getReasearchAreas = asyncHandler(
+export const getHighlights = asyncHandler(
 	async (req: Request, res: Response) => {
 		const highlights = await prisma.highlight.findMany({
 			orderBy: {
 				createdAt: "desc",
 			},
 		});
-		if (!highlights)
-			throw new AppError("Research Areas are not found", 400);
-		response(res, 200, "Research Areas fetched successfully", {
+		if (!highlights) throw new AppError("Highlights are not found", 400);
+		response(res, 200, "Highlights fetched successfully", {
 			highlights,
 		});
 	}
