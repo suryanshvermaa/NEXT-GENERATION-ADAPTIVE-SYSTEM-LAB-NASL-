1a933c4b96a49f214be06a2fc0cbf41e558a1053
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-06-17 13:18:26 +0530

    updated

--- backend/src/controllers/research/researchAreas.controller.ts
+++ backend/src/controllers/research/researchAreas.controller.ts
@@ -99,10 +99,10 @@ export const getResearchArea = asyncHandler(
 				id: Number(id),
 			},
 		});
-		researchArea!.imageURL=await signedUrl(researchArea?.imageURL!,5);
+		const imageURL=await signedUrl(researchArea?.imageURL!,5);
 		if (!researchArea) throw new AppError("researchArea not found", 400);
 		response(res, 200, "researchArea fetched successfully", {
-			researchArea,
+			researchArea:{...researchArea,imageURL},
 		});
 	}
 );
@@ -124,9 +124,9 @@ export const getReasearchAreas = asyncHandler(
 		});
 		if (!researchAreas)
 			throw new AppError("Research Areas are not found", 400);
-		researchAreas.map(async(rs)=>{
+		for(let rs of researchAreas){
 			rs.imageURL=await signedUrl(rs.imageURL,5);
-		})
+		}
 		response(res, 200, "Research Areas fetched successfully", {
 			researchAreas,
 		});
