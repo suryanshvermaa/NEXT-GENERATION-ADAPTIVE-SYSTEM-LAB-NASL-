f5b3ec19905955a65fc73d79dd2d73fe32d85357
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-07-02 23:50:55 +0530

    refactor: update getToken method to include redirect_uri

--- backend/src/auth/googleAuth.ts
+++ backend/src/auth/googleAuth.ts
@@ -4,11 +4,14 @@ import axios from "axios";
 const oauth2Client = new google.auth.OAuth2(
 	process.env.CLIENT_ID!,
 	process.env.CLIENT_SECRET!,
-	"postmessage"
+	"http://localhost:5500/htmlFiles/users/googleCallback.html"
 );
 
 export const getUserInfo = async (code: string) => {
-	const { tokens } = await oauth2Client.getToken(code);
+	const { tokens } = await oauth2Client.getToken({
+		code:code,
+		redirect_uri:"http://localhost:5500/htmlFiles/users/googleCallback.html"
+	});
 	oauth2Client.setCredentials(tokens);
 	const { data } = await axios.get(
 		`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${tokens.access_token}`
