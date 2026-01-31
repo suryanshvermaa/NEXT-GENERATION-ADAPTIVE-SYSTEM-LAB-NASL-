d2d9696ba8c0e1311d485c0975a281b3d4a333a3
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2026-01-31 11:57:40 +0530

    fix: correct authorId initialization and improve userId validation in personal.html

--- backend/src/routes/book.routes.ts
+++ backend/src/routes/book.routes.ts
@@ -15,9 +15,10 @@ const bookRouter = Router();
 bookRouter
 	.post("/create", auth, createBook)
 	.get("/get-all", getAllBooks) // query params: page, limit
+	.get("/get-all-by-user-id/:userId?", getAllBooksByUserId) // params: userId (optional), query params: page, limit
 	.get("/:id", getBookById)
 	.put("/:id", auth, updateBook)
 	.delete("/:id",auth, deleteBook)
-	.get("/get-all-by-user-id", getAllBooksByUserId); // query params: page, limit
+	;
 
 export default bookRouter;
